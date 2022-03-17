00f4b12e66098a9006d24b9dbcc8a299b995bceb
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-03-17 18:37:08 -0500

    Update 1: 15% progress.

---
+++ solutions.js
@@ -0,0 +1,65 @@
+"use strict";
+
+/**
+ * Write your solutions here.
+ *
+ * **Note**: While normally it is good practice to wrap your javascript in an
+ * immediately invoked function expression (iife), you should _not_ do that
+ * here. If you do, the automated tests will not be able to see your functions.
+ * If you don't know what an iffe is, don't worry about it :)
+ *
+ * This file is for defining the following functions. See README.md for more detail.
+ * isTrue
+ * isFalse
+ * not
+ * addOne
+ * isEven
+ * isIdentical
+ * isEqual
+ * or
+ * and
+ * concat
+ */
+// isTrue
+var userInput1 = 1 === 1;
+
+function isTrue(a) {
+    if (a === true) {
+        return  true;
+    } else {
+        return false;
+    }
+};
+isTrue(userInput1);
+
+// isFalse
+var userInput2 = 1 === 1;
+
+function isFalse(b) {
+    if (b === false) {
+        return  true;
+    } else {
+        return false;
+    }
+};
+isTrue(userInput2);
+
+// not
+
+
+//addOne
+var userInput4;
+function addOne(c) {
+    if (c < 0) {
+        return c + 1;
+    } else if ( c > 0) {
+        return c + 1;
+    } else if ( c === "-5") {
+        return
+    }
+    return userInput4;
+};
+
+addOne(-5);
+
+
